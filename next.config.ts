eab62e4bf3c7f4225e1913b46f72a3204cf795cf
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-06-28 18:41:21 +0800

    configure next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    staleTimes: {
+      dynamic: 60 * 5,
+      static: 3600,
+    },
+  },
 };
 
 export default nextConfig;
